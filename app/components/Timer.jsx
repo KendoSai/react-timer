e3d0009130313c34182fd5d340bc035005819543
Khanh Ma <mvkkhanh@gmail.com>
2017-06-02 16:57:43 +0700

    Add navigation component

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <span>Timer.jsx</span>
+            </div>
+        );
+    }
+});
+
+module.exports = Timer;
